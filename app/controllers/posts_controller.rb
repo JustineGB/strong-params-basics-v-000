616599921f133dde053cfd9db6a734547be187d3
Justine B <justinegayton@gmail.com>
2018-12-20 21:56:43 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,7 +27,8 @@ class PostsController < ApplicationController
 	  @post = Post.find(params[:id])
 	end
 	
-	private
+
+private
  
   def post_params
     params.require(:post).permit(:title, :description)
